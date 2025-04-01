9d1936794b784db2ca4f7911d99d8cf7fe37ce49
DESKTOP_ANDREA\andre <andrea.rantin.01.23@gmail.com>
2025-04-01 21:13:19 +0200

    Added missing documentation on system clock init

--- inc/rcc_driver/rcc_driver.h
+++ inc/rcc_driver/rcc_driver.h
@@ -11,6 +11,11 @@
  */
 void RCC_AHB_Enable(RCCAHBEnable peripheral);
 
+/**
+ * @brief Initializes system clock by setting clock source as PLL from HSI to 72MhZ
+ * 
+ * @return @ref RCCStatusCode RCC_OK if successful or an error if there was an error setting up the clock 
+ */
 RCCStatusCode System_Clock_Init();
 
 #endif
