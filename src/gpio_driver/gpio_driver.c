d7f46f763bafe9c659aea54139151784a579c5e5
Andrea_Rantin <andrea.rantin@innuvatech.com>
2025-04-01 17:19:36 +0200

    Merged GPIO changes into dev and tested blink firmware.

---
+++ src/gpio_driver/gpio_driver.c
@@ -0,0 +1,85 @@
+#include "gpio_driver/gpio_driver.h"
+#include "gpio_driver/gpio_internal.h"
+#include <stddef.h>
+
+/*
+    Declare it  here and extern it in header because i wanted the address to stay private
+    in gpio_internal.h but i wanted to expose GPIOA to allow customization
+*/
+GPIO* GPIOA = NULL;
+
+void GPIO_Init(GPIOPortEnum GPIO_Port_Enum) {
+    switch(GPIO_Port_Enum) {
+        case GPIO_PORT_A:
+            GPIOA = ((GPIO*)GPIO_BASE_ADDRESS);
+            break;
+        case GPIO_PORT_B:
+        case GPIO_PORT_C:
+        case GPIO_PORT_D:
+        case GPIO_PORT_E:
+        case GPIO_PORT_F:
+        case GPIO_PORT_G:
+        case GPIO_PORT_H:
+        default:
+        break;
+    }
+}
+
+void GPIO_Set_Mode(GPIO* GPIO_Port, GPIOPinNumber pinNumber, GPIOMode mode) {
+
+    /*
+    Calculates how much i should shift to access the correct pin(each pin has 2 bits)
+    */
+    uint8_t pinNumberBits = (2 * pinNumber);
+
+    /*
+     Clears the register preemptively. If the selected mode
+     was INPUT then it will stay cleared.
+    */
+    GPIO_Port->MODER &= ~(3U << pinNumberBits);
+
+    if(mode == GPIO_MODE_OUTPUT) {
+        GPIO_Port->MODER |= (1U << pinNumberBits);
+    }
+}
+
+void GPIO_Set_PullUp_PullDown(GPIO* GPIO_Port, GPIOPinNumber pinNumber, GPIOPupPdown pullMode) {
+
+    /*
+    Calculates how much i should shift to access the correct pin(each pin has 2 bits)
+    */
+    uint8_t pinNumberBits = (2 * pinNumber);
+
+    //Clears the register preemptively
+    GPIO_Port->PUPDR &= ~(3U << pinNumberBits);
+
+    switch(pullMode) {
+        case GPIO_PULL_DOWN:
+            GPIO_Port->PUPDR |= (2U << (pinNumberBits));
+            break;
+        case GPIO_PULL_UP:
+            GPIO_Port->PUPDR |= (1U << pinNumberBits);
+            break;
+        case GPIO_NO_PULL_UP_PULL_DOWN:
+            break;
+        default:
+            break;
+    }
+}
+
+void GPIO_Toggle_Pin(GPIO* GPIO_Port, GpioPin pin) {
+    GPIO_Port->ODR ^= pin;
+}
+
+uint8_t GPIO_Read_Pin(GPIO* GPIO_Port, GpioPin pin) {
+    return (uint8_t)((GPIO_Port->IDR & pin) != 0);
+}
+
+void GPIO_Write_Pin(GPIO* GPIO_Port, GpioPin pin, GPIOState state) {
+
+    if (state == GPIO_SET) {
+        GPIO_Port->ODR |= pin;
+    } else {
+        GPIO_Port->ODR &= ~pin;
+    }
+}
--- src/main.c
+++ src/main.c
@@ -1,7 +1,18 @@
+#include "gpio_driver/gpio_driver.h"
+#include "rcc_driver/rcc_driver.h"
+#include "gpio_driver/gpio_internal.h"
+
 int main(void) {
 
-    while(1)  {
+    RCC_AHB_Enable(RCC_ENABLE_GPIOA);
 
+    GPIO_Init(GPIO_PORT_A);
+    
+    GPIO_Set_Mode(GPIOA,PIN_N_5,GPIO_MODE_OUTPUT);
+
+    while(1)  {
+        GPIO_Toggle_Pin(GPIOA,PIN_5);
+        for(uint32_t i = 0; i < 100000; i++) {};
     }
     
     return 0;
