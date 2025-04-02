72e573dd93728332a70f19930883cf5aa0d8ee86
Andrea_Rantin <andrea.rantin@innuvatech.com>
2025-04-02 17:39:23 +0200

    Added USART2 initialization in main. Still need to finish it

--- src/main.c
+++ src/main.c
@@ -1,12 +1,15 @@
 #include "gpio_driver/gpio_driver.h"
 #include "rcc_driver/rcc_driver.h"
+#include "uart_driver/uart_driver.h"
+#include "common/global_defs.h"
 
 int main(void) {
 
-   RCCStatusCode checkError =  System_Clock_Init();
-    if(checkError != RCC_OK) {
-        int a = 0;
-        a++;
+    CheckError checkError = {0};
+
+    checkError.rccStatusCode =  System_Clock_Init();
+    if(checkError.rccStatusCode != RCC_OK) {
+        while(1);
     }
 
     RCC_AHB_Enable(RCC_ENABLE_GPIOA);
@@ -14,6 +17,12 @@ int main(void) {
     GPIO_Init(GPIO_PORT_A);
     
     GPIO_Set_Mode(GPIOA,PIN_N_5,GPIO_MODE_OUTPUT);
+
+    //USART 2 INITIALIZATION
+    checkError.uartStatusCode = UART2_Init_TX();
+    if(checkError.uartStatusCode != UART_OK) {
+        while(1);
+    }
 
     while(1)  {
         GPIO_Toggle_Pin(GPIOA,PIN_5);
